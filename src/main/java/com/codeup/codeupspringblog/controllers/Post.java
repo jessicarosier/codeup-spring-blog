268f9fc24b535f8026bead0bc3f1f2437150c5b9
Jessica Rosier <jessica.c.rosier1@gmail.com>
2023-11-18 21:40:57 -0600

    complete with refactoring PostController

--- src/main/java/com/codeup/codeupspringblog/controllers/Post.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/Post.java
@@ -1,12 +1,15 @@
-package com.codeup.codeupspringblog.models;
+package com.codeup.codeupspringblog.controllers;//package com.codeup.codeupspringblog.models;
 
-import jakarta.persistence.Entity;
-import org.springframework.data.annotation.Id;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+
+
 
 
 public class Post {
 
-//    Create a com.codeup.codeupspringblog.models.Post class. This class will ultimately represent a POST record from our database. The class should have private properties and getters and setters for a title and body.
 
     private String title;
     private String body;
--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -1,31 +1,31 @@
 package com.codeup.codeupspringblog.controllers;
-
-import com.codeup.codeupspringblog.models.Post;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
 
+@Controller
 public class PostController {
-
-//    Edit your PostController class to return the views index and show
-//    Inside the method that shows an individual post, create a new post object and pass it to the view.
-//    Inside the method that shows all the posts, create a new array list and add two post objects to it, then pass that list to the view.
-//    In these two pages, you should display information based on the data passed from the controller.
-
 
 
     @GetMapping("/posts")
-    @ResponseBody
-    public String getIndexPage() {
-        return "index";
+    public String getIndexPage(Model model) {
+        Post post1 = new Post("The greatest Coffee tragedy of all time ", "This morning I was making coffee and I forgot to put the coffee pot under the coffee maker. I made a huge mess.");
+        Post post2 = new Post("Traveling to the moon", "I am going to the moon next week. I am very excited about it.");
+        List<Post> posts = new ArrayList<>();
+        posts.add(post1);
+        posts.add(post2);
+        model.addAttribute("posts", posts);
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String getSinglePost(Model model) {
+    public String getSinglePost(Model model, @PathVariable String id) {
 
         Post post = new Post("title", "body");
         model.addAttribute("post", post);
-        return "show";
+        return "posts/show";
 
     }
 

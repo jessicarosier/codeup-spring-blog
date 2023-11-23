4d0dc3d59b8f5afb5546860cb0c12dbdc1058b10
Jessica Rosier <jessica.c.rosier1@gmail.com>
2023-11-23 11:40:55 -0600

    working with rest api controller

--- src/main/resources/static/js/show.js
+++ src/main/resources/static/js/show.js
@@ -1,23 +1,37 @@
 const numLikes = document.getElementById("num-likes");
 
-
 const postId = numLikes.getAttribute("data");
 console.log(postId);
 
+const userLikes = document.getElementById("user-likes");
 
-// Path: src/main/resources/static/js/show.js
-    (function($) {
-        let request = $.ajax({'url': '/posts.ajax'});
-        request.done(function (posts) {
-            var html = '';
-            posts.forEach(function(post) {
-                html += '<div>';
-                html += '<h1>' + post.title + '</h1>';
-                html += '<p>' + post.description + '</p>';
-                html += '<p>Published by ' + post.user.username + '</p>';
-                html += '</div>';
-            });
-            $('#ads').html(html);
-        });
-    })(jQuery);
+let url = `http://localhost:8080/api/posts/likes/${postId}`;
+let options = {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json",
+    },
+};
+
+async function getUserLikes() {
+    let response = await fetch(url, options);
+    let data = await response.json();
+    return data;
+}
+
+numLikes.addEventListener("click", async function () {
+    let users = await getUserLikes();
+    console.log(users);
+    let userString = "";
+    users.forEach(user => {
+        userString += `<li>${user.username}</li>`;
+    });
+    userLikes.innerHTML = userString;
+});
+
+
+
+
+
+
 
